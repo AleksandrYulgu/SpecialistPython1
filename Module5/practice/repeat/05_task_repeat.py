2d998b823ce08d48ea090b8d32836c172f778b26
AleksandrYulgu <88583852+AleksandrYulgu@users.noreply.github.com>
2021-08-28 12:16:34 +0300

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,14 @@
+
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
+import math
 
+def pagination(num_items, items_on_page):
+    num_page = math.ceil(num_items / items_on_page)
+    return num_page
+
+def count_items (num_items, items_on_page):
+
+    return num_items - (pagination(num_items, items_on_page)-1)*items_on_page
+
+print(count_items(11, 6))
