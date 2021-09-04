668e380cd3058798a5c003c2a34f2e1b9d266dc0
AleksandrYulgu <88583852+AleksandrYulgu@users.noreply.github.com>
2021-09-04 16:32:54 +0300

    Update 04_task_struct.py

--- Module8/practice/structs/04_task_struct.py
+++ Module8/practice/structs/04_task_struct.py
@@ -2,3 +2,9 @@
 dictionary_1 = {'a': 300, 'b': 400}
 dictionary_2 = {'c': 500, 'd': 600}
 # Объедините их в один
+
+#uni_dict = {**dictionary_2, **dictionary_2}
+#print(uni_dict)
+uni_dict2 = dictionary_1.copy()
+uni_dict2.update(dictionary_2)
+print(uni_dict2)
