4f0c9c0437b52b84315398b8206916fb4a7703cd
AleksandrYulgu <88583852+AleksandrYulgu@users.noreply.github.com>
2021-09-04 15:31:51 +0300

    Update 02_task_struct.py

--- Module8/practice/structs/02_task_struct.py
+++ Module8/practice/structs/02_task_struct.py
@@ -5,3 +5,23 @@
 #   например, lst = [1, 2, 4, 5, 6, 2, 5, 2], нужно получить lst2 = [1, 2, 4, 5, 6]
 #   2. элементы исходного списка, которые не имеют повторений(встречаются только один раз):
 #   например, lst = [1 , 2, 4, 5, 6, 2, 5, 2], нужно получить lst2 = [1, 4, 6]
+
+import random
+def gen_list(n, m=-10, k=30):
+    list = []
+    for _ in range(10):
+        list.append(random.randint(m, k))
+    return list
+
+lst = [1, 2, 4, 5, 6, 2, 5, 2]
+lst2 = [1 , 2, 4, 5, 6, 2, 5, 2]
+
+def short_list(lst2):
+    new_list = []
+    for num in set(lst2):
+        if lst2.count(num) == 1:
+            new_list.append(num)
+    return new_list
+
+print(set(lst))
+print(short_list(lst2))
