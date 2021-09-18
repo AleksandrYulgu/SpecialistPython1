34b6b1c52f57d1c7fea049adc8384581a4b9d1aa
AleksandrYulgu <88583852+AleksandrYulgu@users.noreply.github.com>
2021-09-18 15:01:11 +0300

    Update 01_task_hard.py

--- Module8/practice/hard/01_task_hard.py
+++ Module8/practice/hard/01_task_hard.py
@@ -5,3 +5,40 @@
 # а значениями кол-во банкнот. Если пользователь запросил некорректную сумму,
 # нужно вывести дружественное сообщение об ошибке.
 # Результат работы программы - текстовый отчет о номиналах и количестве купюр.
+
+
+try:
+    give_out = int(input('Введите, какую сумму выдать: '))
+except ValueError:
+    print('Введите сумму цифрами')
+    give_out = int(input('Введите, какую сумму выдать: '))
+
+
+def count_bills(summ):
+    count_5000 = summ//5000
+    count_2000 = (summ - 5000*count_5000)//2000
+    count_1000 = (summ - (5000*count_5000+2000*count_2000))//1000
+    count_500 = (summ - (5000*count_5000 + 2000*count_2000 + 1000*count_1000))//500
+    remainder = summ - (5000*count_5000 + 2000*count_2000 + 1000*count_1000 + 500*count_500)
+    lst_bils = ("Купюра 5000 ", "Купюра 2000 ", "Купюра 1000 ", "Купюра 500 ")
+    lst_count = []
+    while True:
+        if remainder == 0:
+            lst_count.append(count_5000)
+            lst_count.append(count_2000)
+            lst_count.append(count_1000)
+            lst_count.append(count_500)
+            print (create_dict_bills(lst_bils, lst_count))
+            break
+
+        else:
+            print("Сумма запрашиваемая Вами не может быть выдана. Введите сумму кратную 500 рублей.")
+            give_out = int(input('Введите, какую сумму выдать: '))
+            count_bills(give_out)
+            break
+
+
+def create_dict_bills(lst1, lst2):
+    return dict(zip(lst1, lst2))
+
+count_bills(give_out)
